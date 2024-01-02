452ff9f3dc2c9f0e7cd51604722baa2d9e722f8b
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-01-02 11:56:33 -0500

    Agregar comentario - Bind campos / Uso de ReactiveFormsModule-FormGroup-FormBuilder-Validators todo lo anterior para el uso de formularios

--- src/app/components/agregar-editar-comentario/agregar-editar-comentario.component.ts
+++ src/app/components/agregar-editar-comentario/agregar-editar-comentario.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comentario } from 'src/app/interfaces/Comentario';
 
 @Component({
   selector: 'app-agregar-editar-comentario',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./agregar-editar-comentario.component.css']
 })
 export class AgregarEditarComentarioComponent {
+  agregarComentario: FormGroup;
 
+  constructor(private fb: FormBuilder){
+  this.agregarComentario = this.fb.group({
+    titulo: ['', Validators.required],
+    creador: ['', Validators.required],
+    texto: ['', Validators.required]
+  })
+  }
+
+  agregar(){
+    console.log(this.agregarComentario);
+
+    //Acceder a los valores del formulario AgregarComentario
+  const comentario: Comentario = {
+    titulo: this.agregarComentario.get('titulo')?.value,
+    creador: this.agregarComentario.get('creador')?.value,
+    texto: this.agregarComentario.get('texto')?.value,
+    fechaCreacion: new Date
+  }
+  console.log(comentario);
+  }
 }
