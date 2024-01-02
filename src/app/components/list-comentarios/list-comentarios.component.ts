e58ebb96225d046299cd22e1c87ef0d08a0d627e
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-01-02 10:38:27 -0500

    List Comentario

--- src/app/components/list-comentarios/list-comentarios.component.ts
+++ src/app/components/list-comentarios/list-comentarios.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Comentario } from 'src/app/interfaces/Comentario';
 
 @Component({
   selector: 'app-list-comentarios',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-comentarios.component.css']
 })
 export class ListComentariosComponent {
-
+listComentarios: Comentario[]=[
+  { titulo:'Angular', creador:'Fernando', fechaCreacion: new Date(), texto:"Framework para crear SPA" },
+  { titulo:'React', creador:'Miguel', fechaCreacion: new Date(), texto:"Librería para crear SPA" }
+]
 }
