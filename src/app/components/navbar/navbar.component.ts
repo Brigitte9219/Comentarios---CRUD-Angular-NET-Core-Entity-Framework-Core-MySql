cbd7f31a2e19b2546b608578da698b060426d69c
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-01-02 10:18:49 -0500

    Navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  img = 'https://www.liblogo.com/img-logo/an271a55f-angular-logo-angular-logo-transparent-png-stickpng.png';
 
 }
