1489e09cbacb693707336ab592aa1dc817bf235e
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-01-04 08:10:13 -0500

    Actualizar comentario - parte 1

--- src/app/components/agregar-editar-comentario/agregar-editar-comentario.component.ts
+++ src/app/components/agregar-editar-comentario/agregar-editar-comentario.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Comentario } from 'src/app/interfaces/Comentario';
 import { ComentarioService } from 'src/app/services/comentario.service';
 
@@ -11,19 +11,41 @@ import { ComentarioService } from 'src/app/services/comentario.service';
 })
 export class AgregarEditarComentarioComponent {
   agregarComentario: FormGroup;
+  accion = 'Agregar';
+  id = 0;
+  comentario: Comentario | undefined;
 
   constructor(private fb: FormBuilder,
               private _comentarioService: ComentarioService,//se llama el servicio
-              private router: Router){//para movernos entre componentes
+              private router: Router,//se usa para movernos entre componentes
+              private aRoute: ActivatedRoute){//Se usa para extraer el id
   this.agregarComentario = this.fb.group({
     titulo: ['', Validators.required],
     creador: ['', Validators.required],
-    texto: ['', Validators.required]
+    texto: ['', Validators.required],
   })
+  this.id = +this.aRoute.snapshot.paramMap.get('id')!;
   }
 
   ngOnInit():void {
+  this.esEditar();
+  }
 
+  //Cambia el nombre de Agregar a Editar cuando se da click en "Editar" y adicional muestra los datos en el formulario para poder editarlos
+  esEditar(){
+  if (this.id !== 0){
+    this.accion = 'Editar';
+    this._comentarioService.getComentario(this.id).subscribe(data => {
+      console.log(data);
+      this.agregarComentario.patchValue({
+        titulo: data.titulo,
+        texto: data.texto,
+        creador: data.creador,
+      })
+    }, error => {
+      console.log(error);
+    })
+  }
   }
 
   agregar(){
@@ -36,7 +58,7 @@ export class AgregarEditarComentarioComponent {
     texto: this.agregarComentario.get('texto')?.value,
     fechaCreacion: new Date
   }
-  
+
   this._comentarioService.saveComentario(comentario).subscribe(data => {
   this.router.navigate(['/']);
   }, error => {
